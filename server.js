18591d125798f1d155c82d9da9cfdcbfbdd2deb7
shivamsk76 <st7692833267@gmail.com>
2020-10-21 16:58:46 +0530

    frseshhhhhhhhhh

--- server.js
+++ server.js
@@ -8,12 +8,4 @@ const PORT = process.env.port || 8080
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'form', 'build')))
 
-app.get('/', function(req,res)
-{
-    let testvar = process.env.test
-    console.log("shivam",testvar);
-    res.send("You are Connected")
-    res.render("okay")
-})
-
 app.listen(PORT , console.log("server is running",PORT))
