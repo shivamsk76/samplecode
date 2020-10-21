94977227e5f66f74c21aa5c7f35e795db074783f
unknown <st7692833267@gamil.com>
2020-10-21 12:15:33 +0530

    sample

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+var app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+
+
+const PORT = process.env.port || 8080
+app.use(bodyParser.json())
+
+app.get("/", function(req,res)
+{
+    console.log("okay");
+    res.send("You are Connected")
+    res.render("okay")
+})
+
+app.listen(PORT , console.log("server is running",PORT))
